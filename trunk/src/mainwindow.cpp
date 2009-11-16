22f42fd8715dff6bc3d21f52308cf462b17c34a0
cpulvermacher <cpulvermacher@40b2bbaa-6b6d-0410-8b8d-a22c41bebbaa>
2009-11-16 14:22:45 +0000

    using QString for "About" dialog simplifies things

--- trunk/src/mainwindow.cpp
+++ trunk/src/mainwindow.cpp
@@ -150,17 +150,13 @@ void MainWindow::setUnsavedChanges(bool b)
 
 void MainWindow::showAboutDialog()
 {
-	char text[330] = "\0";
-	strcat(text, "<center><h1>");
-	strcat(text, GRAPHEM_VERSION);
-	strcat(text, "</h1>\
+	QMessageBox::about(this, tr("About Graphem"),
+	tr("<center><h1>%1</h1>\
 <h3>A small mouse gesture based authentication program and screen locker</h3>\
 <p>Documentation is available on <a href=\"http://graphem.berlios.de/\">http://graphem.berlios.de</a></p>\
 <small><p>&copy;2009 Christian Pulvermacher &lt;pulvermacher@gmx.de&gt;</p></small></center>\
-<p>%1</p></small>");
-
-	QMessageBox::about(this, tr("About Graphem"),
-	tr(text)
+<p>%2</p></small>")
+	.arg(GRAPHEM_VERSION)
 	.arg("This program is free software; you can redistribute it and/or modify<br> it under the terms of the GNU General Public License as published by<br> the Free Software Foundation; either version 2 of the License, or<br> (at your option) any later version."));
 }
 
